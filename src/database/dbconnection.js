a1279421d64b93e686a491cabd19c49e1c5499e0
gowoc <g.owoc@student.avans.nl>
2022-07-03 23:54:31 +0200

    last try

--- src/database/dbconnection.js
+++ src/database/dbconnection.js
@@ -1,17 +1,20 @@
 const mysql = require('mysql2');
 require("dotenv").config();
 
-const pool = mysql.createPool({
+const dbConfig = {
+    connectionLimit: 10,
+    waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
-    waitForConnections: true,
     multipleStatements: true,
     host: process.env.DB_HOST,
-    post: process.env.DB_PORT,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-});
+}
+
+const pool = mysql.createPool(dbConfig);
 
 pool.on('acquire', (connection) => {
     console.log('connection %d acquired', connection.threadId)
